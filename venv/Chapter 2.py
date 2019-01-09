7f08b5a58a5f15bca68dac20fef2e02cd69b30f9
Inderjeet <Pan3sar@gmail.com>
2019-01-09 21:48:54 +0000

    Chapter 2

--- venv/Chapter 2.py
+++ venv/Chapter 2.py
@@ -4,29 +4,31 @@ import time
 ledpin = 17
 buttonpin = 18
 
+
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(ledpin, GPIO.OUT, initial=0)
-    GPIO.setup(buttonpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(buttonpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def loop():
     while True:
-        if GPIO.input(buttonpin)==GPIO.LOW:
+        if GPIO.input(buttonpin) == GPIO.LOW:
             GPIO.output(ledpin, GPIO.HIGH)
             print ('...led on')
         else:
             GPIO.output(ledpin, GPIO.LOW)
             print ('led off...')
 
+
 def destroy():
     GPIO.output(ledpin, GPIO.LOW)
     GPIO.cleanup()
+
 
 if __name__ == '__main__':
     setup()
     try:
         loop()
     except KeyboardInterrupt:
-
         destroy()
