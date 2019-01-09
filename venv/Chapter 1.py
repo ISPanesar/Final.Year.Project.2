089132d5fc84d2ab0871d60b2a1ade09b805622f
Inderjeet <Pan3sar@gmail.com>
2019-01-09 16:14:45 +0000

    LED Blinker program

---
+++ venv/Chapter 1.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+ledpin = 17
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(ledpin, GPIO.OUT, initial=0)
+    print('using pin%d'%ledpin)
+
+def loop():
+    while True:
+        GPIO.output(ledpin, GPIO.HIGH)
+        print ('...led on')
+        time.sleep(1)
+        GPIO.output(ledpin, GPIO.LOW)
+        print ('led off...')
+        time.sleep(1)
+
+def destroy():
+    GPIO.output(ledpin, GPIO.LOW)
+    GPIO.cleanup()
+
+if__name__=='__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+
+        destroy()
