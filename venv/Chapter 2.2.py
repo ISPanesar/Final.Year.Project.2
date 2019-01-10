0371e1c6bb83437513fd3d19b23105e316ed6e5f
Inderjeet <Pan3sar@gmail.com>
2019-01-10 10:56:27 +0000

    Chapter 2.2

---
+++ venv/Chapter 2.2.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+
+ledPin = 17
+buttonPin = 18
+ledState = False
+
+
+def setup():
+    print('Program is starting...')
+    GPIO.setmode(GPIO.BCM)              #This is the printed circuit numbers
+    GPIO.setup(ledPin, GPIO.OUT)        #Set ledPin mode as an output
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)      #Set buttonPin mode as an input
+
+
+def buttonEvent(channel):
+    global ledState
+    print ('buttonEvent GPIO%d' %channel)
+    ledState = not ledState
+    if ledState:
+        print ('Turn on LED...')
+    else:
+        print ('Turn off LED')
+    GPIO.output(ledPin,ledState)
+
+
+def loop():
+    GPIO.add_event_detect(buttonPin, GPIO.FALLING, callback = buttonEvent, bouncetime=300)
+    while True:
+        pass
+
+
+def destroy():
+    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.cleanup()
+
+
+if __name__ == '__main__' :
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
+
