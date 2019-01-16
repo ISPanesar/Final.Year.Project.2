b15956cf250f5582b66e9f61d80df294d7b32841
ISPanesar <46325131+ISPanesar@users.noreply.github.com>
2019-01-16 12:40:51 +0000

    Update motor testing.py

--- venv/motor testing.py
+++ venv/motor testing.py
@@ -1,36 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-def setup():
-    x = 0
+x = 0
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(21, GPIO.IN)
-    GPIO.setup(20, GPIO.IN)
-    GPIO.setup(16, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(16, GPIO.OUT)
+p = GPIO.PWM(16, 100)
+p.start(0)
 
-    p = GPIO.PWM(16, 100)
-    p.start(0)
 
 def loop():
     while True:
         p.ChangeDutyCycle(x)
-        if(GPIO.input(26) == False):
-            if(x<50):
+        if (GPIO.input(26) == False):
+            if (x < 50):
                 print('POWERRRRR')
                 x = x + 1
                 time.sleep(0.2)
-            if(GPIO.input(19) == False):
-                if(x > 0):
+            if (GPIO.input(19) == False):
+                if (x > 0):
                     print('SLOWWWWW')
                     x = x - 1
                     time.sleep(0.2)
 
+
 if __name__ == '__main__':
-    setup()
     print('Program is starting')
     try:
         loop()
 
     except KeyboardInterrupt:
         GPIO.cleanup
+
