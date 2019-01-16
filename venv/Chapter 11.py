8e1efed83b788f71e25f20bd55bb6153e77c7464
ISPanesar <Pan3sar@gmail.com>
2019-01-16 15:03:11 +0000

    fixed

--- venv/Chapter 11.py
+++ venv/Chapter 11.py
@@ -10,6 +10,9 @@ CWStep = (0x08, 0x04, 0x02, 0x01)
 def setup():
     print('Program is starting...')
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(38, GPIO.IN)
+    GPIO.setup(40, GPIO.IN)
+    x = 7
     for pin in motorPins:
         GPIO.setup(pin, GPIO.OUT)
 
@@ -38,7 +41,16 @@ def motorStop():
 
 def loop():
     while True:
-        moveSteps(1, 1, 512)
+        if (GPIO.input(40) == False):
+            if (x > 1):
+                x = x - 1
+                print('Speeding up')
+        if (GPIO.input(38) == False):
+            if (x < 15):
+                x = x + 1
+                print('Slowing down')
+
+        moveSteps(1, x, 512)
         time.sleep(0.001)
 
 
