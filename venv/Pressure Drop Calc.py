dfc3a04fe59fa239852bff61ab73ff099a577383
ISPanesar <Pan3sar@gmail.com>
2019-01-25 11:24:48 +0000

    stupid program this is

--- venv/Pressure Drop Calc.py
+++ venv/Pressure Drop Calc.py
@@ -1,7 +1,7 @@
 my_pressures = ['Pressure']
 my_forces = ['Force']
 my_volumetric_flowrates = ['Volumetric Flowrate']
-fluid_viscosity = ['Fluid Viscosity']
+Tube_Diameter = ['Tube Length']
 
 
 def loop():
@@ -19,20 +19,20 @@ def loop():
     Volumetric_Flowrate = Symbol('Q')
     Force = Symbol('F')
     Ds = 0.01
-    Dt = 0.01
-    mu = 0.01
+    Dt = 0.0001
+    mu = 5
     SR = 1
-    L = 0.01
-    #while (mu <= 5):
-    DelP = 4 * L * mu * SR / Dt
-    F = DelP / (pi * (Dt/2) ** 2)
-    Q = pi * (Dt/2) ** 3 * SR / 4
-        #my_pressures.append(DelP)
-        #my_forces.append(F)
-        #my_volumetric_flowrates.append(Q)
-        #fluid_viscosity.append(mu)
-        #mu = mu + 0.0005
-    print(F)
+    L = 2
+    while (Dt <= 0.02):
+        DelP = 4 * L * mu * SR / Dt
+        F = DelP * (pi * (Dt/2) ** 2)
+        Q = pi * (Dt/2) ** 3 * SR / 4
+        my_pressures.append(DelP)
+        my_forces.append(F)
+        my_volumetric_flowrates.append(Q)
+        Tube_Diameter.append(Dt)
+        Dt = Dt + 0.0001
+    print(DelP)
 
 if __name__ == '__main__':
 
@@ -40,13 +40,13 @@ if __name__ == '__main__':
         loop()
     except KeyboardInterrupt:
         quit(0)
-    #final_array = [my_pressures, fluid_viscosity, my_forces, my_volumetric_flowrates]
+    final_array = [my_pressures, Tube_Diameter, my_forces, my_volumetric_flowrates]
 
-    #import csv
+    import csv
 
-    #with open('data.csv', 'w') as csvFile:
-    #    writer = csv.writer(csvFile)
-    #   writer.writerows(final_array)
+    with open('data.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(final_array)
 
-    #csvFile.close()
+    csvFile.close()
     quit(0)
