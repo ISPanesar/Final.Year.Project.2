2519dc6df29f0caf2a732c390ce7ce0450cf3687
ISPanesar <Pan3sar@gmail.com>
2019-01-30 15:26:34 +0000

    I hate this project

--- venv/Weight Sensor.py
+++ venv/Weight Sensor.py
@@ -12,7 +12,7 @@ def cleanAndExit():
     sys.exit()
 
 
-hx = HX711(38, 40)
+hx = HX711(5, 6)
 
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
