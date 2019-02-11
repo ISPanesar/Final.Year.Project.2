ea2ffcb3551a44714bb56db7692a22e85e311a5d
ISPanesar <Pan3sar@gmail.com>
2019-02-11 15:56:17 +0000

    updated to include encoder

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -5,8 +5,8 @@ import time
 
 def setup():
     GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
+    #GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def loop():
     global count
@@ -14,8 +14,8 @@ def loop():
     global rotationtime
     rotationtime = 0
     while True:
-        if ((GPIO.input(6) == True) and (GPIO.input(5) == False)) or\
-                ((GPIO.input(6) == False) and (GPIO.input(5) == True)):
+        """and (GPIO.input(5) == False)) or\((GPIO.input(6) == False) and (GPIO.input(5) == True))"""
+        if GPIO.input(5) == True:
             currenttime = time.time()
             print('one rotation')
             count = count + 1
