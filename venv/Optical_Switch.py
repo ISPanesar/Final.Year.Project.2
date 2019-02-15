955ca96db4269909ac5fb0d64831537ae76392dd
ISPanesar <Pan3sar@gmail.com>
2019-02-15 10:53:53 +0000

    updated to make event driven

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -8,7 +8,6 @@ def setup():
     GPIO.setup(5, GPIO.IN) # Set pull up to high level(3.3V)
     GPIO.setup(6, GPIO.IN)
     GPIO.add_event_detect(5, GPIO.RISING)
-    GPIO.add_event_detect(6, GPIO.RISING)
 
 
 
