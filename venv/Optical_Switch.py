3311537ee13bed2b8ecf655f6062e85253d22573
ISPanesar <Pan3sar@gmail.com>
2019-02-15 11:17:27 +0000

    added bounce times

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -7,7 +7,7 @@ def setup():
     GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers
     GPIO.setup(5, GPIO.IN) # Set pull up to high level(3.3V)
     GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(5, GPIO.FALLING, bouncetime=300)
+    GPIO.add_event_detect(5, GPIO.FALLING, bouncetime=10000)
     GPIO.add_event_detect(6, GPIO.RISING)
 
 
