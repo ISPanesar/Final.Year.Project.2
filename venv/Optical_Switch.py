c42de09f387a1591c058f5309ce79c263d1d7620
ISPanesar <Pan3sar@gmail.com>
2019-02-21 10:49:17 +0000

    fixed if statement to try

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -7,7 +7,7 @@ def setup():
     GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers
     GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
     GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(5, GPIO.FALLING, bouncetime = 20)
+    GPIO.add_event_detect(5, GPIO.RISING, bouncetime = 20)
     GPIO.add_event_detect(6, GPIO.FALLING, bouncetime = 20)
 
 
