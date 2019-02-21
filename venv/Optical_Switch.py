e384e80b094852d8854c48347dcf80cc0f195be8
ISPanesar <Pan3sar@gmail.com>
2019-02-21 10:56:17 +0000

    fixed if statement to try

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -6,34 +6,7 @@ import time
 
 
 def loop():
-    global count
-    count = 0
-    global rotationtime
-    currenttime = time.time()
-    while True:
-        """and (GPIO.input(5) == False)) or\((GPIO.input(6) == False) and (GPIO.input(5) == True))"""
-        if GPIO.event_detected(5):
-            #and GPIO.input(6) == True:
-            print('event detected')
-            destroy()
-        #if GPIO.input(5) == False:
-         #   print('False')
-        else:
-            print('Python hates me')
-            '''count = count + 1
-            rotationtime = time.time() - currenttime
-            print('The count is %s' % count)
-            time.sleep(0.01)
-        elif time.time() - currenttime > 60:
-            RPM = count/(time.time()-currenttime)*60/2
-            print(str(RPM))
-            currenttime = time.time()
-            count = 0
-        else:
-            print('Clear')
-            time.sleep(0.01)
-        # print('The count is %s, the step time is %s ' % (count, rotationtime))
-'''
+
 def destroy():
     GPIO.cleanup() # Release resource
 
@@ -45,8 +18,34 @@ if __name__ == '__main__': # Set the Program start from here
     GPIO.add_event_detect(5, GPIO.BOTH)
     GPIO.add_event_detect(6, GPIO.FALLING, bouncetime = 20)
     try:
-
-        loop()
+        global count
+        count = 0
+        global rotationtime
+        currenttime = time.time()
+        while True:
+            """and (GPIO.input(5) == False)) or\((GPIO.input(6) == False) and (GPIO.input(5) == True))"""
+            if GPIO.event_detected(5):
+                # and GPIO.input(6) == True:
+                print('event detected')
+                destroy()
+            # if GPIO.input(5) == False:
+            #   print('False')
+            else:
+                print('Python hates me')
+                '''count = count + 1
+                rotationtime = time.time() - currenttime
+                print('The count is %s' % count)
+                time.sleep(0.01)
+            elif time.time() - currenttime > 60:
+                RPM = count/(time.time()-currenttime)*60/2
+                print(str(RPM))
+                currenttime = time.time()
+                count = 0
+            else:
+                print('Clear')
+                time.sleep(0.01)
+            # print('The count is %s, the step time is %s ' % (count, rotationtime))
+    '''
     except KeyboardInterrupt: # When pressed 'Ctrl+C' child program destroy() will be executed.
         destroy()
 
