ab2ebe4ae2a460b0637329b2b8782b9104caa3ac
ISPanesar <Pan3sar@gmail.com>
2019-02-21 10:54:58 +0000

    fixed if statement to try

--- venv/Optical_Switch.py
+++ venv/Optical_Switch.py
@@ -3,13 +3,6 @@ import RPi.GPIO as GPIO
 import time
 
 
-def setup():
-    GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(5, GPIO.BOTH)
-    GPIO.add_event_detect(6, GPIO.FALLING, bouncetime = 20)
-
 
 
 def loop():
@@ -46,7 +39,11 @@ def destroy():
 
 
 if __name__ == '__main__': # Set the Program start from here
-    setup()
+    GPIO.setmode(GPIO.BCM) # Set GPIO as PIN Numbers
+    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pull up to high level(3.3V)
+    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(5, GPIO.BOTH)
+    GPIO.add_event_detect(6, GPIO.FALLING, bouncetime = 20)
     try:
 
         loop()
