1980b014edb7ac3f62d7c4f8d007a2f5fca41a7c
ISPanesar <Pan3sar@gmail.com>
2019-02-28 10:21:35 +0000

    other variables not global

--- Multithreading learning.py
+++ Multithreading learning.py
@@ -287,7 +287,7 @@ def initialise():
     pi = pigpio.pi()
     if not pi.connected:
         exit(0)
-    global s
+    global s, c, mode, reading
     s = HX711.sensor(
         pi, DATA=20, CLOCK=21, mode=HX711.CH_B_GAIN_32)
 
