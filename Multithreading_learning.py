91174584eeb2e652fb660bce9b770919e04bdbdb
ISPanesar <Pan3sar@gmail.com>
2019-03-28 15:18:34 +0000

    trying to fix the rpm measuremnt issues and the trackrate issues as the list was being emptied each loop cause i'm a dolt

--- Multithreading_learning.py
+++ Multithreading_learning.py
@@ -444,7 +444,7 @@ def forceloop():
         while not que2.empty():
             RPMmeas = que2.get()
             RPM = RPMmeas[0]
-            starttime = RPMmeas[1]
+
 
         if count != c:
             c = count
